6e8fc0b5df7bbc137a910453b3b6e67e7ed5bf91
nikolan <94351312+nikolan123@users.noreply.github.com>
2024-03-24 09:15:47 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+import logging
+from hostsedit import *
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+app = Flask(__name__)
+app.logger.disabled = True
+log.disabled = True
+    
+@app.route('/consumeKey/', methods=['GET'])
+@app.route('/drm/consumeKey/', methods=['GET'])
+def drmpc():
+    response = f"status=1&type={request.args.get('types')}"
+    print(f"Got new request from {request.remote_addr}!\nGET {request.base_url}\nKey: {request.args.get('key')}\nTypes: {request.args.get('types')}\nUDID: {request.args.get('udid')}\nReturning {response}, 200")
+    return response
+
+if __name__ == '__main__':
+    print(addrovio("127.0.0.1"))
+    app.run(debug=True, host="127.0.0.1", port=80)
